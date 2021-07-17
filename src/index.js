e4a99122b1c00be21a83914d985abdec0e432a44
Marko Kungla <marko.kungla@gmail.com>
2021-07-17 15:24:38 +0300

    update deps

--- src/index.js
+++ src/index.js
@@ -4,21 +4,8 @@ if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available. Did you include A-Frame?')
 }
 
-const ARACVER = require('../package.json').version
-
-const log = AFRAME.utils.debug
-// const error = log('A-Frame Resonance Audio Component:error')
-const info = log('A-Frame Resonance Audio Component:info')
-const warn = log('A-Frame Resonance Audio Component:warn')
-
 if (module.hot) {
   module.hot.accept()
-}
-
-if (process.env.NODE_ENV === 'production') {
-  info(`Version: ${ARACVER}`)
-} else {
-  warn(`Version: ${ARACVER}-dev`)
 }
 
 require('./resonance-audio-room')
